fe7153af986e2f6163a3e3435923e0615525d56d
Rafal Gumienny <guma44@gmail.com>
2016-01-25 17:03:15 +0100

    Dropdown menu for recipes is working

--- create_test_recipes.py
+++ create_test_recipes.py
@@ -1,15 +1,20 @@
 from PiBrew import Recipe, Step
 
+found_objects = Recipe.objects(name__contains='test')
+for ob in found_objects:
+    print "Deleting ", ob.name
+    ob.delete()
+
 s1 = Step(name='step1_-1_50', span=-1, temperature=50)
 s2 = Step(name='step2_-1_70', span=-1, temperature=70)
 s3 = Step(name='step2_1_70', span=1, temperature=70)
 s4 = Step(name='step3_1_80', span=1, temperature=80)
 
 
-# r1 = Recipe(name='test1', slug='test1', steps=[s1, s2])
-# r2 = Recipe(name='test2', slug='test2', steps=[s1, s3])
+r1 = Recipe(name='test1', slug='test1', steps=[s1, s2])
+r2 = Recipe(name='test2', slug='test2', steps=[s1, s3])
 r3 = Recipe(name='test3', slug='test3', steps=[s1, s3, s4])
 
-# r1.save()
-# r2.save()
+r1.save()
+r2.save()
 r3.save()
